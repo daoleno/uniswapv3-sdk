cb49b4dfea1a43678f5600e3ff3b4eae2e18cb39
daoleno <daodaoonline@gmail.com>
2022-01-08 23:00:44 +0800

    Support maxLiquidityForAmounts utils.

---
+++ utils/max_liquidity_for_amounts.go
@@ -0,0 +1,93 @@
+package utils
+
+import (
+	"math/big"
+
+	"github.com/daoleno/uniswapv3-sdk/constants"
+)
+
+/**
+ * Returns an imprecise maximum amount of liquidity received for a given amount of token 0.
+ * This function is available to accommodate LiquidityAmounts#getLiquidityForAmount0 in the v3 periphery,
+ * which could be more precise by at least 32 bits by dividing by Q64 instead of Q96 in the intermediate step,
+ * and shifting the subtracted ratio left by 32 bits. This imprecise calculation will likely be replaced in a future
+ * v3 router contract.
+ * @param sqrtRatioAX96 The price at the lower boundary
+ * @param sqrtRatioBX96 The price at the upper boundary
+ * @param amount0 The token0 amount
+ * @returns liquidity for amount0, imprecise
+ */
+func maxLiquidityForAmount0Imprecise(sqrtRatioAX96, sqrtRatioBX96, amount0 *big.Int) *big.Int {
+	if sqrtRatioAX96.Cmp(sqrtRatioBX96) > 0 {
+		sqrtRatioAX96, sqrtRatioBX96 = sqrtRatioBX96, sqrtRatioAX96
+	}
+	intermediate := new(big.Int).Div(new(big.Int).Mul(sqrtRatioAX96, sqrtRatioBX96), constants.Q96)
+	return new(big.Int).Div(new(big.Int).Mul(amount0, intermediate), new(big.Int).Sub(sqrtRatioBX96, sqrtRatioAX96))
+}
+
+/**
+ * Returns a precise maximum amount of liquidity received for a given amount of token 0 by dividing by Q64 instead of Q96 in the intermediate step,
+ * and shifting the subtracted ratio left by 32 bits.
+ * @param sqrtRatioAX96 The price at the lower boundary
+ * @param sqrtRatioBX96 The price at the upper boundary
+ * @param amount0 The token0 amount
+ * @returns liquidity for amount0, precise
+ */
+func maxLiquidityForAmount0Precise(sqrtRatioAX96, sqrtRatioBX96, amount0 *big.Int) *big.Int {
+	if sqrtRatioAX96.Cmp(sqrtRatioBX96) > 0 {
+		sqrtRatioAX96, sqrtRatioBX96 = sqrtRatioBX96, sqrtRatioAX96
+	}
+	numerator := new(big.Int).Mul(new(big.Int).Mul(amount0, sqrtRatioAX96), sqrtRatioBX96)
+	denominator := new(big.Int).Mul(constants.Q96, new(big.Int).Sub(sqrtRatioBX96, sqrtRatioAX96))
+	return new(big.Int).Div(numerator, denominator)
+}
+
+/**
+ * Computes the maximum amount of liquidity received for a given amount of token1
+ * @param sqrtRatioAX96 The price at the lower tick boundary
+ * @param sqrtRatioBX96 The price at the upper tick boundary
+ * @param amount1 The token1 amount
+ * @returns liquidity for amount1
+ */
+func maxLiquidityForAmount1(sqrtRatioAX96, sqrtRatioBX96, amount1 *big.Int) *big.Int {
+	if sqrtRatioAX96.Cmp(sqrtRatioBX96) > 0 {
+		sqrtRatioAX96, sqrtRatioBX96 = sqrtRatioBX96, sqrtRatioAX96
+	}
+	return new(big.Int).Div(new(big.Int).Mul(amount1, constants.Q96), new(big.Int).Sub(sqrtRatioBX96, sqrtRatioAX96))
+}
+
+/**
+ * Computes the maximum amount of liquidity received for a given amount of token0, token1,
+ * and the prices at the tick boundaries.
+ * @param sqrtRatioCurrentX96 the current price
+ * @param sqrtRatioAX96 price at lower boundary
+ * @param sqrtRatioBX96 price at upper boundary
+ * @param amount0 token0 amount
+ * @param amount1 token1 amount
+ * @param useFullPrecision if false, liquidity will be maximized according to what the router can calculate,
+ * not what core can theoretically support
+ */
+func maxLiquidityForAmounts(sqrtRatioCurrentX96 *big.Int, sqrtRatioAX96, sqrtRatioBX96 *big.Int, amount0, amount1 *big.Int, useFullPrecision bool) *big.Int {
+	if sqrtRatioAX96.Cmp(sqrtRatioBX96) > 0 {
+		sqrtRatioAX96, sqrtRatioBX96 = sqrtRatioBX96, sqrtRatioAX96
+	}
+	var maxLiquidityForAmount0 func(*big.Int, *big.Int, *big.Int) *big.Int
+	if useFullPrecision {
+		maxLiquidityForAmount0 = maxLiquidityForAmount0Precise
+	} else {
+		maxLiquidityForAmount0 = maxLiquidityForAmount0Imprecise
+	}
+	if sqrtRatioCurrentX96.Cmp(sqrtRatioAX96) <= 0 {
+		return maxLiquidityForAmount0(sqrtRatioAX96, sqrtRatioBX96, amount0)
+	} else if sqrtRatioCurrentX96.Cmp(sqrtRatioBX96) < 0 {
+		liquidity0 := maxLiquidityForAmount0(sqrtRatioCurrentX96, sqrtRatioBX96, amount0)
+		liquidity1 := maxLiquidityForAmount1(sqrtRatioAX96, sqrtRatioCurrentX96, amount1)
+		if liquidity0.Cmp(liquidity1) < 0 {
+			return liquidity0
+		}
+		return liquidity1
+
+	} else {
+		return maxLiquidityForAmount1(sqrtRatioAX96, sqrtRatioBX96, amount1)
+	}
+}
---
+++ utils/max_liquidity_for_amounts_test.go
@@ -0,0 +1,252 @@
+package utils
+
+import (
+	"math/big"
+	"reflect"
+	"testing"
+
+	"github.com/daoleno/uniswap-sdk-core/entities"
+)
+
+func Test_maxLiquidityForAmounts(t *testing.T) {
+	type args struct {
+		sqrtRatioCurrentX96 *big.Int
+		sqrtRatioAX96       *big.Int
+		sqrtRatioBX96       *big.Int
+		amount0             *big.Int
+		amount1             *big.Int
+		useFullPrecision    bool
+	}
+	lgamounts0, _ := new(big.Int).SetString("1214437677402050006470401421068302637228917309992228326090730924516431320489727", 10)
+	lgamounts1, _ := new(big.Int).SetString("1214437677402050006470401421098959354205873606971497132040612572422243086574654", 10)
+	lgamounts2, _ := new(big.Int).SetString("1214437677402050006470401421082903520362793114274352355276488318240158678126184", 10)
+	tests := []struct {
+		name string
+		args args
+		want *big.Int
+	}{
+		{
+			name: "imprecise - price inside - 100 token0, 200 token1",
+			args: args{
+				EncodeSqrtRatioX96(big.NewInt(1), big.NewInt(1)),
+				EncodeSqrtRatioX96(big.NewInt(100), big.NewInt(110)),
+				EncodeSqrtRatioX96(big.NewInt(110), big.NewInt(100)),
+				big.NewInt(100),
+				big.NewInt(200),
+				false,
+			},
+			want: big.NewInt(2148),
+		},
+		{
+			name: "imprecise - price inside - 100 token0, max token1",
+			args: args{
+				EncodeSqrtRatioX96(big.NewInt(1), big.NewInt(1)),
+				EncodeSqrtRatioX96(big.NewInt(100), big.NewInt(110)),
+				EncodeSqrtRatioX96(big.NewInt(110), big.NewInt(100)),
+				big.NewInt(100),
+				entities.MaxUint256,
+				false,
+			},
+			want: big.NewInt(2148),
+		},
+		{
+			name: "imprecise - price inside - max token0, 200 token1",
+			args: args{
+				EncodeSqrtRatioX96(big.NewInt(1), big.NewInt(1)),
+				EncodeSqrtRatioX96(big.NewInt(100), big.NewInt(110)),
+				EncodeSqrtRatioX96(big.NewInt(110), big.NewInt(100)),
+				entities.MaxUint256,
+				big.NewInt(200),
+				false,
+			},
+			want: big.NewInt(4297),
+		},
+		{
+			name: "imprecise - price below - 100 token0, 200 token1",
+			args: args{
+				EncodeSqrtRatioX96(big.NewInt(99), big.NewInt(110)),
+				EncodeSqrtRatioX96(big.NewInt(100), big.NewInt(110)),
+				EncodeSqrtRatioX96(big.NewInt(110), big.NewInt(100)),
+				big.NewInt(100),
+				big.NewInt(200),
+				false,
+			},
+			want: big.NewInt(1048),
+		},
+		{
+			name: "imprecise - price below - 100 token0, max token1",
+			args: args{
+				EncodeSqrtRatioX96(big.NewInt(99), big.NewInt(110)),
+				EncodeSqrtRatioX96(big.NewInt(100), big.NewInt(110)),
+				EncodeSqrtRatioX96(big.NewInt(110), big.NewInt(100)),
+				big.NewInt(100),
+				entities.MaxUint256,
+				false,
+			},
+			want: big.NewInt(1048),
+		},
+		{
+			name: "imprecise - price below - max token0, 200 token1",
+			args: args{
+				EncodeSqrtRatioX96(big.NewInt(99), big.NewInt(110)),
+				EncodeSqrtRatioX96(big.NewInt(100), big.NewInt(110)),
+				EncodeSqrtRatioX96(big.NewInt(110), big.NewInt(100)),
+				entities.MaxUint256,
+				big.NewInt(200),
+				false,
+			},
+			want: lgamounts0,
+		},
+		{
+			name: "imprecise - price above - 100 token0, 200 token1",
+			args: args{
+				EncodeSqrtRatioX96(big.NewInt(111), big.NewInt(100)),
+				EncodeSqrtRatioX96(big.NewInt(100), big.NewInt(110)),
+				EncodeSqrtRatioX96(big.NewInt(110), big.NewInt(100)),
+				big.NewInt(100),
+				big.NewInt(200),
+				false,
+			},
+			want: big.NewInt(2097),
+		},
+		{
+			name: "imprecise - price above - 100 token0, max token1",
+			args: args{
+				EncodeSqrtRatioX96(big.NewInt(111), big.NewInt(100)),
+				EncodeSqrtRatioX96(big.NewInt(100), big.NewInt(110)),
+				EncodeSqrtRatioX96(big.NewInt(110), big.NewInt(100)),
+				big.NewInt(100),
+				entities.MaxUint256,
+				false,
+			},
+			want: lgamounts1,
+		},
+		{
+			name: "imprecise - price above - max token0, 200 token1",
+			args: args{
+				EncodeSqrtRatioX96(big.NewInt(111), big.NewInt(100)),
+				EncodeSqrtRatioX96(big.NewInt(100), big.NewInt(110)),
+				EncodeSqrtRatioX96(big.NewInt(110), big.NewInt(100)),
+				entities.MaxUint256,
+				big.NewInt(200),
+				false,
+			},
+			want: big.NewInt(2097),
+		},
+		{
+			name: "precise - price inside - 100 token0, 200 token1",
+			args: args{
+				EncodeSqrtRatioX96(big.NewInt(1), big.NewInt(1)),
+				EncodeSqrtRatioX96(big.NewInt(100), big.NewInt(110)),
+				EncodeSqrtRatioX96(big.NewInt(110), big.NewInt(100)),
+				big.NewInt(100),
+				big.NewInt(200),
+				true,
+			},
+			want: big.NewInt(2148),
+		},
+		{
+			name: "precise - price inside - 100 token0, max token1",
+			args: args{
+				EncodeSqrtRatioX96(big.NewInt(1), big.NewInt(1)),
+				EncodeSqrtRatioX96(big.NewInt(100), big.NewInt(110)),
+				EncodeSqrtRatioX96(big.NewInt(110), big.NewInt(100)),
+				big.NewInt(100),
+				entities.MaxUint256,
+				true,
+			},
+			want: big.NewInt(2148),
+		},
+		{
+			name: "precise - price inside - max token0, 200 token1",
+			args: args{
+				EncodeSqrtRatioX96(big.NewInt(1), big.NewInt(1)),
+				EncodeSqrtRatioX96(big.NewInt(100), big.NewInt(110)),
+				EncodeSqrtRatioX96(big.NewInt(110), big.NewInt(100)),
+				entities.MaxUint256,
+				big.NewInt(200),
+				true,
+			},
+			want: big.NewInt(4297),
+		},
+		{
+			name: "precise - price below - 100 token0, 200 token1",
+			args: args{
+				EncodeSqrtRatioX96(big.NewInt(99), big.NewInt(110)),
+				EncodeSqrtRatioX96(big.NewInt(100), big.NewInt(110)),
+				EncodeSqrtRatioX96(big.NewInt(110), big.NewInt(100)),
+				big.NewInt(100),
+				big.NewInt(200),
+				true,
+			},
+			want: big.NewInt(1048),
+		},
+		{
+			name: "precise - price below - 100 token0, max token1",
+			args: args{
+				EncodeSqrtRatioX96(big.NewInt(99), big.NewInt(110)),
+				EncodeSqrtRatioX96(big.NewInt(100), big.NewInt(110)),
+				EncodeSqrtRatioX96(big.NewInt(110), big.NewInt(100)),
+				big.NewInt(100),
+				entities.MaxUint256,
+				true,
+			},
+			want: big.NewInt(1048),
+		},
+		{
+			name: "precise - price below - max token0, 200 token1",
+			args: args{
+				EncodeSqrtRatioX96(big.NewInt(99), big.NewInt(110)),
+				EncodeSqrtRatioX96(big.NewInt(100), big.NewInt(110)),
+				EncodeSqrtRatioX96(big.NewInt(110), big.NewInt(100)),
+				entities.MaxUint256,
+				big.NewInt(200),
+				true,
+			},
+			want: lgamounts2,
+		},
+		{
+			name: "precise - price above - 100 token0, 200 token1",
+			args: args{
+				EncodeSqrtRatioX96(big.NewInt(111), big.NewInt(100)),
+				EncodeSqrtRatioX96(big.NewInt(100), big.NewInt(110)),
+				EncodeSqrtRatioX96(big.NewInt(110), big.NewInt(100)),
+				big.NewInt(100),
+				big.NewInt(200),
+				true,
+			},
+			want: big.NewInt(2097),
+		},
+		{
+			name: "precise - price above - 100 token0, max token1",
+			args: args{
+				EncodeSqrtRatioX96(big.NewInt(111), big.NewInt(100)),
+				EncodeSqrtRatioX96(big.NewInt(100), big.NewInt(110)),
+				EncodeSqrtRatioX96(big.NewInt(110), big.NewInt(100)),
+				big.NewInt(100),
+				entities.MaxUint256,
+				true,
+			},
+			want: lgamounts1,
+		},
+		{
+			name: "precise - price above - max token0, 200 token1",
+			args: args{
+				EncodeSqrtRatioX96(big.NewInt(111), big.NewInt(100)),
+				EncodeSqrtRatioX96(big.NewInt(100), big.NewInt(110)),
+				EncodeSqrtRatioX96(big.NewInt(110), big.NewInt(100)),
+				entities.MaxUint256,
+				big.NewInt(200),
+				true,
+			},
+			want: big.NewInt(2097),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := maxLiquidityForAmounts(tt.args.sqrtRatioCurrentX96, tt.args.sqrtRatioAX96, tt.args.sqrtRatioBX96, tt.args.amount0, tt.args.amount1, tt.args.useFullPrecision); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("maxLiquidityForAmounts() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
