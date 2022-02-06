9d5cdeaabf55881bfea3a896adb0c7594ea4daf7
daoleno <daodaoonline@gmail.com>
2022-02-06 10:01:17 +0800

    Support payments.

---
+++ periphery/payments.go
@@ -0,0 +1,57 @@
+package periphery
+
+import (
+	_ "embed"
+	"encoding/json"
+	"math/big"
+
+	"github.com/daoleno/uniswap-sdk-core/entities"
+	"github.com/ethereum/go-ethereum/accounts/abi"
+	"github.com/ethereum/go-ethereum/common"
+)
+
+//go:embed contracts/interfaces/IPeripheryPaymentsWithFee.sol/IPeripheryPaymentsWithFee.json
+var paymentsABI []byte
+
+type FeeOptions struct {
+	Fee       *entities.Percent // The percent of the output that will be taken as a fee.
+	Recipient common.Address    // The recipient of the fee.
+
+}
+
+func encodeFeeBips(fee *entities.Percent) *big.Int {
+	return fee.Multiply(entities.NewPercent(big.NewInt(10000), big.NewInt(1))).Quotient()
+}
+
+func getPaymentsABI() abi.ABI {
+	var wabi WrappedABI
+	err := json.Unmarshal(paymentsABI, &wabi)
+	if err != nil {
+		panic(err)
+	}
+	return wabi.ABI
+}
+
+func EncodeUnwrapWETH9(amountMinimum *big.Int, recipient common.Address, feeOptions *FeeOptions) ([]byte, error) {
+	abi := getPaymentsABI()
+	if feeOptions != nil {
+		return abi.Pack("unwrapWETH9WithFee", amountMinimum, &recipient, encodeFeeBips(feeOptions.Fee), feeOptions.Recipient)
+	}
+
+	return abi.Pack("unwrapWETH9", amountMinimum, recipient)
+}
+
+func EncodeSweepToken(token *entities.Token, amountMinimum *big.Int, recipient common.Address, feeOptions *FeeOptions) ([]byte, error) {
+	abi := getPaymentsABI()
+
+	if feeOptions != nil {
+		return abi.Pack("sweepTokenWithFee", token.Address, amountMinimum, recipient, encodeFeeBips(feeOptions.Fee), feeOptions.Recipient)
+	}
+
+	return abi.Pack("sweepToken", token.Address, amountMinimum, recipient)
+}
+
+func EncodeRefundETH() ([]byte, error) {
+	abi := getPaymentsABI()
+	return abi.Pack("refundETH")
+}
---
+++ periphery/payments_test.go
@@ -0,0 +1,52 @@
+package periphery
+
+import (
+	"math/big"
+	"testing"
+
+	"github.com/daoleno/uniswap-sdk-core/entities"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/stretchr/testify/assert"
+)
+
+var (
+	recipient  = common.HexToAddress("0x0000000000000000000000000000000000000003")
+	amount     = big.NewInt(123)
+	feeOptions = &FeeOptions{
+		Fee:       entities.NewPercent(big.NewInt(1), big.NewInt(1000)),
+		Recipient: common.HexToAddress("0x0000000000000000000000000000000000000009"),
+	}
+	token = entities.NewToken(1, common.HexToAddress("0x0000000000000000000000000000000000000001"), 18, "t0", "token0")
+)
+
+func TestEncodeUnwrapWETH9(t *testing.T) {
+	// works without feeOptions
+	calldata, err := EncodeUnwrapWETH9(amount, recipient, nil)
+	assert.NoError(t, err)
+	assert.Equal(t, "0x49404b7c000000000000000000000000000000000000000000000000000000000000007b0000000000000000000000000000000000000000000000000000000000000003", hexutil.Encode(calldata))
+
+	// works with feeOptions
+	calldata, err = EncodeUnwrapWETH9(amount, recipient, feeOptions)
+	assert.NoError(t, err)
+	assert.Equal(t, "0x9b2c0a37000000000000000000000000000000000000000000000000000000000000007b0000000000000000000000000000000000000000000000000000000000000003000000000000000000000000000000000000000000000000000000000000000a0000000000000000000000000000000000000000000000000000000000000009", hexutil.Encode(calldata))
+}
+
+func TestEncodeSweepToken(t *testing.T) {
+	// works without feeOptions
+	calldata, err := EncodeSweepToken(token, amount, recipient, nil)
+	assert.NoError(t, err)
+	assert.Equal(t, "0xdf2ab5bb0000000000000000000000000000000000000000000000000000000000000001000000000000000000000000000000000000000000000000000000000000007b0000000000000000000000000000000000000000000000000000000000000003", hexutil.Encode(calldata))
+
+	// works with feeOptions
+	calldata, err = EncodeSweepToken(token, amount, recipient, feeOptions)
+	assert.NoError(t, err)
+	assert.Equal(t, "0xe0e189a00000000000000000000000000000000000000000000000000000000000000001000000000000000000000000000000000000000000000000000000000000007b0000000000000000000000000000000000000000000000000000000000000003000000000000000000000000000000000000000000000000000000000000000a0000000000000000000000000000000000000000000000000000000000000009", hexutil.Encode(calldata))
+}
+
+func TestEncodeRefundETH(t *testing.T) {
+	// works without feeOptions
+	calldata, err := EncodeRefundETH()
+	assert.NoError(t, err)
+	assert.Equal(t, "0x12210e8a", hexutil.Encode(calldata))
+}
